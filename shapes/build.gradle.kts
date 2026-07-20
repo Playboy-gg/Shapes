78047a271c6fb2a8b90851f30c1acc40d628e384
Ankit Mehta <kr.ankush2024@gmail.com>
2026-07-20 20:50:40 +0530

    Remove Maven publishing configuration
    Removed Maven publishing configuration from build script.

--- shapes/build.gradle.kts
+++ shapes/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.jetbrains.compose)
-    id("com.vanniktech.maven.publish")
 }
 
 kotlin {
@@ -42,35 +41,3 @@ kotlin {
     }
 }
 
-mavenPublishing {
-    publishToMavenCentral()
-    signAllPublications()
-
-    coordinates("io.github.kyant0", "shapes", "1.2.0")
-
-    pom {
-        name.set("Shapes")
-        description.set("iOS-like shapes for Compose Multiplatform")
-        inceptionYear.set("2026")
-        url.set("https://github.com/Kyant0/Shapes")
-        licenses {
-            license {
-                name.set("The Apache License, Version 2.0")
-                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                distribution.set("repo")
-            }
-        }
-        developers {
-            developer {
-                id.set("Kyant0")
-                name.set("Kyant")
-                url.set("https://github.com/Kyant0")
-            }
-        }
-        scm {
-            url.set("https://github.com/Kyant0/Shapes")
-            connection.set("scm:git:git://github.com/Kyant0/Shapes.git")
-            developerConnection.set("scm:git:ssh://git@github.com/Kyant0/Shapes.git")
-        }
-    }
-}
